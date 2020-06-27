87766276a9868d2791427b9b31b720dd0d552382
Jose Hidago <jhidalgo521@gmail.com>
2020-06-27 22:22:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,22 @@
 require 'pry'
 
-class 
+class CashRegister
+  attr_accessor :total, :discount, :price, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  
+
+
+
+
+
+
+
+
+  binding.pry 
+end 
