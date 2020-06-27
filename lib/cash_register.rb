eb9f20b0ae7d6acd10ee63c56c73f9a432f728a9
Jose Hidago <jhidalgo521@gmail.com>
2020-06-27 22:36:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
-    @last_transac = @total 
+   
     if quantity > 1
       counter = 0
       while counter < quantity
